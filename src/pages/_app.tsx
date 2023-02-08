9740bd0f4906e9bf4ab78b24a677006ff649b6a9
Martín <maguera37@gmail.com>
2023-02-08 18:25:57 -0300

    feat: Initial config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component { ...pageProps } />
+  </ChakraProvider>
+)
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+import { Heading } from '@chakra-ui/react'
+
+const Home: NextPage = () => <Heading color="purple.500">aaa</Heading>
+
+export default Home
