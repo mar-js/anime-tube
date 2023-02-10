c127442a2208676292d013cd4f844637f1921428
Martín <maguera37@gmail.com>
2023-02-10 19:47:40 -0300

    feat: I added the slider in aside component

--- src/components/organisms/aside/index.tsx
+++ src/components/organisms/aside/index.tsx
@@ -1,16 +1,22 @@
 import { useAnimesPopular } from 'contexts'
 
 import { GridItem } from '@chakra-ui/react'
-import { AnimePopular } from 'components/molecules'
+import { AnimePopular, Slider } from 'components/molecules'
 
 export const Aside: React.FC = () => {
   const { animes } = useAnimesPopular()
 
   return (
-    <GridItem as="aside" area="aside">
-      { animes?.map((anime) => (
-        <AnimePopular key={ anime.animeId } { ...anime } />
-      )) }
+    <GridItem
+      as="aside"
+      area="aside"
+      overflow="hidden"
+    >
+      <Slider>
+        { animes?.map((anime) => (
+          <AnimePopular key={ anime.animeId } { ...anime } />
+        )) }
+      </Slider>
     </GridItem>
   )
 }
