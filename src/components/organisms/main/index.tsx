3c156dc91508aca6eb97463eaefbad39a2188dce
Martín <maguera37@gmail.com>
2023-02-14 17:55:03 -0300

    feat: I added container-slider in main component

--- src/components/organisms/main/index.tsx
+++ src/components/organisms/main/index.tsx
@@ -1,7 +1,13 @@
 import { GridItem } from '@chakra-ui/react'
+import { ContainerSlider } from 'components/molecules'
 
 export const Main: React.FC = () => (
-  <GridItem as="main" area="main">
-    Main
+  <GridItem
+    as="main"
+    area="main"
+    pt={ 5 }
+    pl={ 5 }
+  >
+    <ContainerSlider />
   </GridItem>
 )
