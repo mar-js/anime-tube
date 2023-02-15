297fed337c2866d3211e625b2e9d5fd652a16fb3
Martín <maguera37@gmail.com>
2023-02-15 17:33:21 -0300

    feat: I added link in card-anime

--- src/components/molecules/card-anime/index.tsx
+++ src/components/molecules/card-anime/index.tsx
@@ -4,30 +4,38 @@ import {
   Img
 } from '@chakra-ui/react'
 
-import { IAnime } from 'interfaces'
+import { IAnimeApiAPi } from 'interfaces'
+import Link from 'next/link'
 
-export const CardAnime: React.FC<IAnime> = ({ animeImg, animeTitle }) => (
-  <Box
-    w="200px"
-    h="250px"
-    position="relative"
-    boxShadow="5px 5px 10px #e1e1e1"
-  >
-    <Img
-      w="100%"
-      h="100%"
-      src={ animeImg }
-      alt={ animeTitle }
-    />
-    <Heading
-      as="span"
-      w="100%"
-      position="absolute"
-      bottom="0"
-      color="white"
-      p={ 2 }
-      fontSize="10px"
-      bgColor="blackAlpha.700"
-    >{ animeTitle }</Heading>
-  </Box>
+export const CardAnime: React.FC<IAnimeApiAPi> = ({
+  animeId,
+  animeImg,
+  animeTitle
+}) => (
+  <Link href={ `anime/${animeId}` }>
+    <Box
+      w="200px"
+      h="250px"
+      position="relative"
+      boxShadow="5px 5px 10px #858585"
+      sx={ { '&:hover': { boxShadow: '5px 5px 15px #3f3f3f' } } }
+    >
+      <Img
+        w="100%"
+        h="100%"
+        src={ animeImg }
+        alt={ animeTitle }
+      />
+      <Heading
+        as="span"
+        w="100%"
+        position="absolute"
+        bottom="0"
+        color="white"
+        p={ 2 }
+        fontSize="10px"
+        bgColor="blackAlpha.700"
+      >{ animeTitle }</Heading>
+    </Box>
+  </Link>
 )
