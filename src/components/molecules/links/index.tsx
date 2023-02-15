0dd1a1fcc931c1c322a40165ca2a386b9aa36169
Martín <maguera37@gmail.com>
2023-02-15 17:32:44 -0300

    fix: I removed const LINKS and import of utils/constants

--- src/components/molecules/links/index.tsx
+++ src/components/molecules/links/index.tsx
@@ -1,28 +1,11 @@
 import { Box, Heading } from '@chakra-ui/react'
 import { Link } from 'components/atoms'
 
-const LINKS = [
-  {
-    id: 1,
-    name: 'Home',
-    url: '/'
-  },
-  {
-    id: 2,
-    name: 'Popular',
-    url: '/popular'
-  },
-  {
-    id: 3,
-    name: 'Movies',
-    url: '/movies'
-  }
-]
+import { LINKS } from 'utils'
 
 export const Links: React.FC = () => (
   <Box
     display="flex"
-    flexDirection="column"
     justifyContent="center"
     alignItems="center"
   >
@@ -35,7 +18,7 @@ export const Links: React.FC = () => (
         key={ id }
         as="h6"
         size="sm"
-        mb={ 5 }
+        ml={ 5 }
         color="white"
       >
         <Link
