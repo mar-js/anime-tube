ccabd81f6f198f35d906cdd303418c7ce67f5c39
Martín <maguera37@gmail.com>
2023-02-15 17:34:03 -0300

    fix: I made the component more reusable

--- src/components/molecules/container-slider/index.tsx
+++ src/components/molecules/container-slider/index.tsx
@@ -1,16 +1,18 @@
 import { Box, Heading } from '@chakra-ui/react'
 import { Slider } from '../slider'
 
-export const ContainerSlider: React.FC = () => (
-  <Box w="100%">
+import { IContainerSlider } from 'interfaces'
+
+export const ContainerSlider: React.FC<IContainerSlider> = ({ title, animes }) => (
+  <Box w="100%" mb={ 10 }>
     <Heading
       as="h2"
       size="md"
       mb={ 3 }
       color="gray.500"
     >
-      Animes Popular:
+      { title }:
     </Heading>
-    <Slider />
+    <Slider animes={ animes } />
   </Box>
 )
