12821679c0f328c822d384b33053dff779d7c5c4
Martín <maguera37@gmail.com>
2023-02-15 17:31:33 -0300

    fix: I removed grid css

--- src/components/templates/home/index.tsx
+++ src/components/templates/home/index.tsx
@@ -1,24 +1,13 @@
-import { Grid } from '@chakra-ui/react'
 import {
-  Aside,
   Header,
   Footer,
   Main
 } from 'components/organisms'
 
 export const LayoutHome: React.FC = () => (
-  <Grid
-    templateAreas={ `
-      'aside header'
-      'aside main'
-      'footer footer'
-    ` }
-    gridTemplateRows="auto 700px auto"
-    gridTemplateColumns="20% 80%"
-  >
+  <>
     <Header />
-    <Aside />
     <Main />
     <Footer />
-  </Grid>
+  </>
 )
