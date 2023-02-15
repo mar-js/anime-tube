dea6766c220011a9d4a1c7fbe31bce3d6a18d11b
Martín <maguera37@gmail.com>
2023-02-15 17:30:41 -0300

    fix: I corrections the interfaces and I added new interfaces

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,20 +1,35 @@
 export type TLoading = 'loading' | 'ok' | 'fail'
 
+export interface IContainerSlider extends IAnimeApi {
+  title: string;
+
+}
+
 export interface ILink {
   path: string;
   text: string;
 }
 
-export interface IDataAnime {
-  loading: TLoading;
-  animes?: IAnime[];
-  error?: string | unknown;
+export interface IAnimeApiHome {
+  newAnime: IDataAnime;
+  moviesAnime: IDataAnime;
+  animePopular: IDataAnime;
 }
 
-export interface IAnime {
+export interface IDataAnime extends IAnimeApi {
+  loading: TLoading;
+  error?: string | unknown;
+  title: string;
+}
+
+export interface IAnimeApiAPi {
   animeId: string;
   animeTitle: string;
   animeImg: string;
   releasedDate: string;
   animeUrl: string;
+}
+
+export interface IAnimeApi {
+  animes?: IAnimeApiAPi[]
 }
