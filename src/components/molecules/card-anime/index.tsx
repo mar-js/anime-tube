e74720445611b6f809625a3d7a2c4762e9781aa3
Martín <maguera37@gmail.com>
2023-02-17 19:02:02 -0300

    feat: I added what the hover be optional

--- src/components/molecules/card-anime/index.tsx
+++ src/components/molecules/card-anime/index.tsx
@@ -4,38 +4,35 @@ import {
   Img
 } from '@chakra-ui/react'
 
-import { IAnimeApiAPi } from 'interfaces'
-import Link from 'next/link'
+import { ICardAnime } from 'interfaces'
 
-export const CardAnime: React.FC<IAnimeApiAPi> = ({
-  animeId,
+export const CardAnime: React.FC<ICardAnime> = ({
   animeImg,
-  animeTitle
+  animeTitle,
+  isHover
 }) => (
-  <Link href={ `anime/${animeId}` }>
-    <Box
-      w="200px"
-      h="250px"
-      position="relative"
-      boxShadow="5px 5px 10px #858585"
-      sx={ { '&:hover': { boxShadow: '5px 5px 15px #3f3f3f' } } }
-    >
-      <Img
-        w="100%"
-        h="100%"
-        src={ animeImg }
-        alt={ animeTitle }
-      />
-      <Heading
-        as="span"
-        w="100%"
-        position="absolute"
-        bottom="0"
-        color="white"
-        p={ 2 }
-        fontSize="10px"
-        bgColor="blackAlpha.700"
-      >{ animeTitle }</Heading>
-    </Box>
-  </Link>
+  <Box
+    w="200px"
+    h="250px"
+    position="relative"
+    boxShadow={ isHover ? '5px 5px 10px #858585' : '' }
+    sx={ { '&:hover': { boxShadow: isHover ? '5px 5px 15px #3f3f3f' : '' } } }
+  >
+    <Img
+      w="100%"
+      h="100%"
+      src={ animeImg }
+      alt={ animeTitle }
+    />
+    <Heading
+      as="span"
+      w="100%"
+      position="absolute"
+      bottom="0"
+      color="white"
+      p={ 2 }
+      fontSize="10px"
+      bgColor="blackAlpha.700"
+    >{ animeTitle }</Heading>
+  </Box>
 )
