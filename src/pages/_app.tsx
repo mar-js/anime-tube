87d3f0fd4271579859b66c0e1479d295363c19cd
Martín <maguera37@gmail.com>
2023-02-17 18:59:44 -0300

    feat: I created new anime page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { Footer, Header } from 'components/organisms'
+
 import 'styles/index.css'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
+    <Header />
     <Component { ...pageProps } />
+    <Footer />
   </ChakraProvider>
 )
 
---
+++ src/pages/anime/[id].tsx
@@ -0,0 +1,28 @@
+import { NextPage, GetServerSideProps } from 'next'
+
+import { AnimeDetailsContext } from 'contexts'
+
+import { getAnimeDetails } from 'apis'
+
+import { LayoutAnime } from 'components/templates'
+
+import { IAnimeApiDetails, IAnimeDetails } from 'interfaces'
+
+const Anime: NextPage<IAnimeApiDetails> = (data) => (
+  <AnimeDetailsContext.Provider value={ data }>
+    <LayoutAnime />
+  </AnimeDetailsContext.Provider>
+)
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  try {
+    const { id } = params as { id: string }
+    const DATA_ANIME_DETAILS = await getAnimeDetails(id) as unknown
+
+    return { props: { details: { ...DATA_ANIME_DETAILS as IAnimeDetails } } }
+  } catch (e) {
+    return { props: { e } }
+  }
+}
+
+export default Anime
