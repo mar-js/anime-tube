59e8e0d80c8331bb52f3d33b7c762a6d1a8a726e
Martín <maguera37@gmail.com>
2023-02-20 19:52:20 -0300

    feat: I finished the design of details anime

--- src/components/organisms/section/index.tsx
+++ src/components/organisms/section/index.tsx
@@ -7,8 +7,11 @@ import {
   ListItem,
   UnorderedList,
   Stack,
-  Text
+  Text,
+  Link
 } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+
 import { CardAnime } from 'components/molecules'
 
 export const Section: React.FC = () => {
@@ -21,31 +24,95 @@ export const Section: React.FC = () => {
     }
   } = useAnimeDetails()
 
-  console.log(episodesList)
-
   return (
-    <Box as="section">
-      <Flex>
+    <Box
+      as="section"
+      py={ 3 }
+      px={ 6 }
+    >
+      <Flex
+        alignItems="center"
+        flexDir={ {
+          base: 'column-reverse',
+          md: 'column-reverse',
+          lg: 'row'
+        } }
+      >
         <CardAnime
+          isDetails
           animeImg={ animeImg }
           animeTitle={ animeTitle }
         />
-        <Stack>
+        <Stack
+          w={ {
+            base: '100%',
+            md: '100%',
+            lg: '65%'
+          } }
+          mb={ {
+            base: 6,
+            md: 6,
+            lg: 0
+          } }
+          pl={ {
+            md: 0,
+            lg: 10
+          } }
+        >
           <Heading as="h2">{ animeTitle }</Heading>
-          <Text fontSize="lg">{ synopsis }</Text>
+          <Text
+            fontSize={ {
+              base: '12px',
+              md: '14px',
+              lg: '16px'
+            } }
+          >{ synopsis || 'This is a movie, there is no description so as not to spoil' }</Text>
         </Stack>
       </Flex>
       { episodesList.length > 0 && (
-        <UnorderedList>
+        <UnorderedList
+          my={ 10 }
+          listStyleType="none"
+        >
           { episodesList.map(({
             episodeId,
             episodeNum,
             episodeUrl
           }) => (
-            <ListItem key={ episodeId }>
-              <a href={ episodeUrl } target="_blank" rel="noreferrer">
-              Episode: { episodeNum }
-              </a>
+            <ListItem
+              key={ episodeId }
+              mb={ 2 }
+              boxShadow="5px 5px 30px #e2d5ff"
+              borderRadius={ 50 }
+              sx={ { '&:hover': { boxShadow: '5px 5px 10px #cdb6ff' } } }
+            >
+              <Link
+                href={ episodeUrl }
+                target="_blank"
+                rel="noreferrer"
+                display="flex"
+                justifyContent="space-between"
+                w="100%"
+                h={ {
+                  base: '50px',
+                  md: '50px',
+                  lg: '70px'
+                } }
+                alignItems="center"
+                px={ {
+                  base: 5,
+                  md: 8,
+                  lg: 10
+                } }
+                fontSize={ {
+                  base: '12px',
+                  md: '15px',
+                  lg: '15px'
+                } }
+              >
+                View episode: { episodeNum }
+                <ExternalLinkIcon />
+              </Link>
             </ListItem>
           )) }
         </UnorderedList>
