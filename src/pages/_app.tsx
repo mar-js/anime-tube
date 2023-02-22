78613cbfff9ee0acfb94c6e74a267cb64a5f8936
Martín <maguera37@gmail.com>
2023-02-22 15:41:15 -0300

    feat: I added provider in root of project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { AnimeSearchProvider } from 'providers'
+
 import { Footer, Header } from 'components/organisms'
 
 import 'styles/index.css'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
-    <Header />
-    <Component { ...pageProps } />
-    <Footer />
+    <AnimeSearchProvider>
+      <Header />
+      <Component { ...pageProps } />
+      <Footer />
+    </AnimeSearchProvider>
   </ChakraProvider>
 )
 
