d854602930e968086a965bb392f11db342109dea
Martín <maguera37@gmail.com>
2023-02-22 16:11:48 -0300

    feat: I added anime-search api

--- src/components/molecules/search/index.tsx
+++ src/components/molecules/search/index.tsx
@@ -1,3 +1,11 @@
+import { FormEvent } from 'react'
+
+import { useAnimeSearch } from 'contexts'
+
+import { getAnimeSearch } from 'apis'
+
+import { getValueForm } from 'helpers'
+
 import {
   Input,
   InputGroup,
@@ -5,31 +13,69 @@ import {
 } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 
-export const Search: React.FC = () => (
-  <InputGroup
-    bgColor="white"
-    mb={ {
-      base: 5,
-      md: 0
-    } }
-    borderRadius={ 50 }
-  >
-    <InputLeftAddon
-      bgColor="transparent"
-      border="none"
-    >
-      <Search2Icon />
-    </InputLeftAddon>
-    <Input
-      type="search"
-      border="none"
-      borderRadius={ 50 }
-      sx={ {
-        '&:focus-visible': {
-          border: 'none',
-          boxShadow: 'none'
-        }
+import { IAnimeApi } from 'interfaces'
+
+export const Search: React.FC = () => {
+  const { setResult } = useAnimeSearch()
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    const VALUE = getValueForm(e)
+
+    setResult({
+      title: 'Results',
+      loading: 'loading'
+    })
+
+    try {
+      const RESULT = await getAnimeSearch(VALUE)
+
+      setResult({
+        title: 'Results',
+        loading: 'ok',
+        animes: RESULT as IAnimeApi[]
+      })
+    } catch (error) {
+      setResult({
+        title: 'Results',
+        loading: 'fail',
+        error
+      })
+    }
+  }
+
+  return (
+    <InputGroup
+      as="form"
+      action="GET"
+      onSubmit={ handleSubmit }
+      bgColor="white"
+      mb={ {
+        base: 5,
+        md: 0
       } }
-    />
-  </InputGroup>
-)
+      borderRadius={ 50 }
+    >
+      <InputLeftAddon
+        bgColor="transparent"
+        border="none"
+      >
+        <Search2Icon />
+      </InputLeftAddon>
+      <Input
+        name="search"
+        type="search"
+        border="none"
+        pl={ 0 }
+        borderRadius={ 50 }
+        sx={ {
+          '&:focus-visible': {
+            border: 'none',
+            boxShadow: 'none'
+          }
+        } }
+      />
+    </InputGroup>
+  )
+}
