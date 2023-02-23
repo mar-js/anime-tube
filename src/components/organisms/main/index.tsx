a1b3f2086e5483c552a360e89bed8418f9cb0395
Martín <maguera37@gmail.com>
2023-02-23 19:18:11 -0300

    feat: I added banner-anime in main and change design

--- src/components/organisms/main/index.tsx
+++ src/components/organisms/main/index.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import { useAnimeHome, useAnimeSearch } from 'contexts'
 
 import { Box } from '@chakra-ui/react'
-import { ContainerSlider } from 'components/molecules'
+import { BannerAnime, ContainerSlider } from 'components/molecules'
 
 export const Main: React.FC = () => {
   const {
@@ -25,15 +25,15 @@ export const Main: React.FC = () => {
   }, [ result ])
 
   return (
-    <Box
-      as="main"
-      pt={ 5 }
-      pl={ 5 }
-    >
+    <Box as="main" pt={ 5 }>
       { result && (
         <ContainerSlider { ...result } />
       ) }
       <ContainerSlider { ...newAnime } />
+      <BannerAnime
+        { ...animePopular.animes[0] }
+        synopsis="Hora de estreno en Latinoamérica: Domingos a la 1:30 AM PST (Hora del Pacífico) Hora de estreno en España: Domingos a las 09:30 (Hora española peninsular) Monkey D. Luffy se niega a que nadie se interponga en su camino para convertirse en Rey de los Piratas. Se lanza a sucar los mares y se convertirá en un capitán dispuesto a no darse por vencido jamás hasta hacerse con el mayor tesoro de la historia: el legendario One Piece."
+      />
       <ContainerSlider { ...animePopular } />
       <ContainerSlider { ...moviesAnime } />
     </Box>
