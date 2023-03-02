e1964cfc098b0e9b387471fa9f4657335e051447
Martín <maguera37@gmail.com>
2023-03-02 03:57:58 -0300

    fix: I removed use-effect and use-state and I added use-get-anime-random  custom hook

--- src/components/organisms/main/index.tsx
+++ src/components/organisms/main/index.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react'
+import { useAnimeHome } from 'contexts'
 
-import { useAnimeHome, useAnimeSearch } from 'contexts'
+import { useGetAnimeRandom, useResultSearchAnime } from 'hooks'
 
 import { Box } from '@chakra-ui/react'
 import { BannerAnime, ContainerSlider } from 'components/molecules'
@@ -11,18 +11,8 @@ export const Main: React.FC = () => {
     moviesAnime,
     newAnime
   } = useAnimeHome()
-  const { result, setResult } = useAnimeSearch()
-
-  useEffect(() => {
-    if ((result?.animes?.length as number) < 1) {
-      setResult({
-        title: 'Result',
-        loading: 'fail',
-        error: 'Not result'
-      })
-    }
-
-  }, [ result?.animes ])
+  const { result } = useResultSearchAnime()
+  const { animeRandom } = useGetAnimeRandom()
 
   return (
     <Box as="main" pt={ 5 }>
@@ -30,11 +20,8 @@ export const Main: React.FC = () => {
         <ContainerSlider { ...result } />
       ) }
       <ContainerSlider { ...newAnime } />
-      <BannerAnime
-        { ...animePopular.animes[0] }
-        synopsis="Hora de estreno en Latinoamérica: Domingos a la 1:30 AM PST (Hora del Pacífico) Hora de estreno en España: Domingos a las 09:30 (Hora española peninsular) Monkey D. Luffy se niega a que nadie se interponga en su camino para convertirse en Rey de los Piratas. Se lanza a sucar los mares y se convertirá en un capitán dispuesto a no darse por vencido jamás hasta hacerse con el mayor tesoro de la historia: el legendario One Piece."
-      />
       <ContainerSlider { ...animePopular } />
+      { animeRandom && <BannerAnime { ...animeRandom } /> }
       <ContainerSlider { ...moviesAnime } />
     </Box>
   )
