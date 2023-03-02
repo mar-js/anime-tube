113b67a024e25fc486b894aa6fb8018f31d5c5a5
Martín <maguera37@gmail.com>
2023-03-02 04:01:42 -0300

    feat: I added spread operator for more styles and I removed text prop

--- src/components/atoms/link/index.tsx
+++ src/components/atoms/link/index.tsx
@@ -4,8 +4,16 @@ import { Link as LinkCH } from '@chakra-ui/react'
 
 import { ILink } from 'interfaces'
 
-export const Link: React.FC<ILink> = ({ text, path }) => (
-  <LinkCH as={ NextLink } href={ path }>
-    { text }
+export const Link: React.FC<ILink> = ({
+  path,
+  children,
+  ...css
+}) => (
+  <LinkCH
+    as={ NextLink }
+    href={ path }
+    { ...css }
+  >
+    { children }
   </LinkCH>
 )
