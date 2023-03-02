e82aef2be96ee360c8ead98982924830a37645f6
Martín <maguera37@gmail.com>
2023-03-02 04:07:32 -0300

    fix: I changed desing of banner-anime in molecules

--- src/components/molecules/banner-anime/index.tsx
+++ src/components/molecules/banner-anime/index.tsx
@@ -3,17 +3,16 @@ import {
   Flex,
   Heading,
   Image,
-  Stack,
   Text
 } from '@chakra-ui/react'
+import { Link } from 'components/atoms'
 
-import { IAnimeDetails } from 'interfaces'
+import { IAnimeApi } from 'interfaces'
 
-export const BannerAnime: React.FC<IAnimeDetails> = ({
+export const BannerAnime: React.FC<IAnimeApi> = ({
+  animeId,
   animeImg,
   animeTitle,
-  synopsis,
-  type,
   releasedDate
 }) => (
   <Box
@@ -26,73 +25,84 @@ export const BannerAnime: React.FC<IAnimeDetails> = ({
       md: '50px 0'
     } }
 
-    bgGradient="linear(to-r, #9022ff, #cfa0ff)"
+    bgGradient={ {
+      base: 'linear(to-b, #9022ff, #ab57ff, #cfa0ff)',
+      md: 'linear(to-r, #9022ff, #ab57ff, #cfa0ff)'
+    } }
   >
-    <Flex
+    <Link
+      path={ `anime/${animeId}` }
       w={ {
         base: '100%',
         md: '85%',
-        lg: '75%'
-      } }
-      flexDir={ {
-        base: 'column',
-        md: 'row'
-      } }
-      p={ 3 }
-      bgColor={ {
-        base: 'transparent',
-        md: 'blackAlpha.500'
-      } }
-      bgGradient={ {
-        base: 'linear(to-t, #ffffff, #ffffff, #ffffff, #ffffff00)',
-        md: 'none'
+        lg: '65%'
       } }
       color={ {
         base: 'black',
         md: 'white'
       } }
     >
-      <Image
-        w={ {
-          base: '180px',
-          md: '80%'
+      <Flex
+        w="100%"
+        flexDir={ {
+          base: 'column',
+          md: 'row'
         } }
-        h={ {
-          base: '100%',
-          md: '300px'
-        } }
-        mt="-25px"
-        mx={ {
-          base: 6,
-          md: 0
-        } }
-        src={ animeImg }
-        alt={ animeTitle }
-      />
-      <Stack
-        p={ 5 }
+        p={ 3 }
         bgColor={ {
-          base: 'white',
-          md: 'transparent'
+          base: 'transparent',
+          md: 'blackAlpha.500'
+        } }
+        bgGradient={ {
+          base: 'linear(to-t, #ffffff, #ffffff, transparent)',
+          md: 'none'
         } }
       >
-        <Heading
-          as="h3"
-          fontSize={ {
-            base: '16px',
-            md: '18px',
-            lg: '20px'
+        <Image
+          w={ {
+            base: '180px',
+            md: '300px'
           } }
-        >{ animeTitle }</Heading>
-        <Text as="span" size="sm">{ type } * { releasedDate }</Text>
-        <Text
-          fontSize={ {
-            base: '12px',
-            md: '15px',
-            lg: '16px'
+          h={ {
+            base: '100%',
+            md: '300px'
           } }
-        >{ synopsis }</Text>
-      </Stack>
-    </Flex>
+          mt="-25px"
+          mx={ {
+            base: 6,
+            md: 0
+          } }
+          src={ animeImg }
+          alt={ animeTitle }
+        />
+        <Flex
+          flexDir="column"
+          justifyContent="center"
+          alignItems="start"
+          p={ 5 }
+          bgColor={ {
+            base: 'white',
+            md: 'transparent'
+          } }
+        >
+          <Heading
+            as="h3"
+            mb={ 2 }
+            fontSize={ {
+              base: '16px',
+              md: '18px',
+              lg: '20px'
+            } }
+          >{ animeTitle }</Heading>
+          <Text
+            fontSize={ {
+              base: '14px',
+              md: '16px',
+              lg: '18px'
+            } }
+          >Release Date: { releasedDate }</Text>
+        </Flex>
+      </Flex>
+    </Link>
   </Box>
 )
