13d684c22dc8cc5562bd878741da70a6e8d024a6
Martín <maguera37@gmail.com>
2023-03-05 03:00:02 -0300

    fix:  I correction components of layouts for be more reusable

--- src/components/organisms/main/index.tsx
+++ src/components/organisms/main/index.tsx
@@ -1,28 +1,9 @@
-import { useAnimeHome } from 'contexts'
-
-import { useGetAnimeRandom, useResultSearchAnime } from 'hooks'
+import { PropsWithChildren } from 'react'
 
 import { Box } from '@chakra-ui/react'
-import { BannerAnime, ContainerSlider } from 'components/molecules'
 
-export const Main: React.FC = () => {
-  const {
-    animePopular,
-    moviesAnime,
-    newAnime
-  } = useAnimeHome()
-  const { result } = useResultSearchAnime()
-  const { animeRandom } = useGetAnimeRandom()
-
-  return (
-    <Box as="main" pt={ 5 }>
-      { result && (
-        <ContainerSlider { ...result } />
-      ) }
-      <ContainerSlider { ...newAnime } />
-      <ContainerSlider { ...animePopular } />
-      { animeRandom && <BannerAnime { ...animeRandom } /> }
-      <ContainerSlider { ...moviesAnime } />
-    </Box>
-  )
-}
+export const Main: React.FC<PropsWithChildren> = ({ children }) => (
+  <Box as="main" pt={ 5 }>
+    { children }
+  </Box>
+)
