a4f80da76e4b4ba346d9fd4dbe2fb9f796b7eb19
Martín <maguera37@gmail.com>
2023-03-05 03:02:15 -0300

    feat: I added components and data consumption

--- src/components/templates/anime/index.tsx
+++ src/components/templates/anime/index.tsx
@@ -1,3 +1,117 @@
+import { useAnimeDetails } from 'contexts'
+
+import {
+  Flex,
+  Heading,
+  ListItem,
+  UnorderedList,
+  Stack,
+  Text,
+  Link
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { CardAnime } from 'components/molecules'
 import { Section } from 'components/organisms'
 
-export const LayoutAnime: React.FC = () => <Section />
+export const LayoutAnime: React.FC = () => {
+  const {
+    details: {
+      animeImg,
+      animeTitle,
+      synopsis,
+      episodesList
+    }
+  } = useAnimeDetails()
+
+  return (
+    <Section>
+      <Flex
+        alignItems="center"
+        flexDir={ {
+          base: 'column-reverse',
+          md: 'column-reverse',
+          lg: 'row'
+        } }
+      >
+        <CardAnime
+          isDetails
+          animeImg={ animeImg }
+          animeTitle={ animeTitle }
+        />
+        <Stack
+          w={ {
+            base: '100%',
+            md: '100%',
+            lg: '70%'
+          } }
+          mb={ {
+            base: 6,
+            md: 6,
+            lg: 0
+          } }
+          pl={ {
+            md: 0,
+            lg: 10
+          } }
+        >
+          <Heading as="h2">{ animeTitle }</Heading>
+          <Text
+            fontSize={ {
+              base: '12px',
+              md: '14px',
+              lg: '16px'
+            } }
+          >{ synopsis || 'This is a movie, there is no description so as not to spoil' }</Text>
+        </Stack>
+      </Flex>
+      { episodesList.length > 0 && (
+        <UnorderedList
+          my={ 10 }
+          listStyleType="none"
+        >
+          { episodesList.map(({
+            episodeId,
+            episodeNum,
+            episodeUrl
+          }) => (
+            <ListItem
+              key={ episodeId }
+              mb={ 2 }
+              boxShadow="5px 5px 30px #e2d5ff"
+              borderRadius={ 50 }
+              sx={ { '&:hover': { boxShadow: '5px 5px 10px #cdb6ff' } } }
+            >
+              <Link
+                href={ episodeUrl }
+                target="_blank"
+                rel="noreferrer"
+                display="flex"
+                justifyContent="space-between"
+                w="100%"
+                h={ {
+                  base: '50px',
+                  md: '50px',
+                  lg: '70px'
+                } }
+                alignItems="center"
+                px={ {
+                  base: 5,
+                  md: 8,
+                  lg: 10
+                } }
+                fontSize={ {
+                  base: '12px',
+                  md: '15px',
+                  lg: '15px'
+                } }
+              >
+                  View episode: { episodeNum }
+                <ExternalLinkIcon />
+              </Link>
+            </ListItem>
+          )) }
+        </UnorderedList>
+      ) }
+    </Section>
+  )
+}
--- src/components/templates/home/index.tsx
+++ src/components/templates/home/index.tsx
@@ -1,3 +1,28 @@
-import { Main } from 'components/organisms'
+import { useAnimeHome } from 'contexts'
 
-export const LayoutHome: React.FC = () => <Main />
+import { useGetAnimeRandom, useResultSearchAnime } from 'hooks'
+
+import { Main } from 'components/organisms'
+import { BannerAnime, ContainerSlider } from 'components/molecules'
+
+export const LayoutHome: React.FC = () => {
+  const {
+    animePopular,
+    moviesAnime,
+    newAnime
+  } = useAnimeHome()
+  const { result } = useResultSearchAnime()
+  const { animeRandom } = useGetAnimeRandom()
+
+  return (
+    <Main>
+      { result && (
+        <ContainerSlider { ...result } />
+      ) }
+      <ContainerSlider { ...newAnime } />
+      <ContainerSlider { ...animePopular } />
+      { animeRandom && <BannerAnime { ...animeRandom } /> }
+      <ContainerSlider { ...moviesAnime } />
+    </Main>
+  )
+}
