ffa067301a7419e2634d468a7525a3391fa5dd20
Martín <maguera37@gmail.com>
2023-03-13 17:36:50 -0300

    feat: I added infinite scroll

--- src/components/templates/navigation-animes/index.tsx
+++ src/components/templates/navigation-animes/index.tsx
@@ -1,17 +1,36 @@
+import { useRouter } from 'next/router'
+
 import { useNavigationAnimes } from 'contexts'
+
+import { useInfiniteScroll } from 'hooks'
 
 import { Section } from 'components/organisms'
 import { Link } from 'components/atoms'
 import { CardAnime } from 'components/molecules'
-import { Grid, GridItem } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  Grid,
+  GridItem,
+  Spinner
+} from '@chakra-ui/react'
 
 export const LayoutNavigationAnimes: React.FC = () => {
+  const { pathname } = useRouter()
   const { animes } = useNavigationAnimes()
+  const {
+    items,
+    loading,
+    hasMore
+  } = useInfiniteScroll({
+    animes,
+    slug: pathname
+  })
 
   return (
     <Section>
       <Grid templateColumns="repeat(auto-fill, minmax(200px, 1fr))" gap={ 2 }>
-        { animes?.map(anime => (
+        { items.map(anime => (
           <GridItem
             key={ anime.animeId }
             w="100%"
@@ -22,6 +41,13 @@ export const LayoutNavigationAnimes: React.FC = () => {
             </Link>
           </GridItem>
         )) }
+        { loading && <Spinner color="purple.500" size="xl" /> }
+        { !hasMore && (
+          <Alert status="info">
+            <AlertIcon />
+            Not more Animes.
+          </Alert>
+        ) }
       </Grid>
     </Section>
   )
