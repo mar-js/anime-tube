9c7dd6e5e63176012edad8ea3f1322178d065b4f
Martín <maguera37@gmail.com>
2023-03-13 17:43:32 -0300

    feat: I created IOnScroll interface

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -8,6 +8,12 @@ import {
 export type TLoading = 'loading' | 'ok' | 'fail'
 export type TGenres = 'action' | 'adventure' | 'cars' | 'comedy' | 'crime' | 'dementia' | 'demons' | 'drama' | 'dub' | 'ecchi' | 'family' | 'fantasy' | 'game' | 'gourmet' | 'harem' | 'historical' | 'horror' | 'josei' | 'kids' | 'magic' | 'martial-arts' | 'mecha' | 'military' | 'Mmusic' | 'mystery' | 'parody' | 'police' | 'psychological' | 'romance' | 'samurai' | 'school' | 'sci-fi' | 'seinen' | 'shoujo' | 'shoujo-ai' | 'shounen' | 'shounen-ai' | 'slice-of-Life' | 'space' | 'sports' | 'super-power' | 'supernatural' | 'suspense' | 'thriller' | 'vampire' | 'yaoi' | 'yuri'
 
+export interface IOnScroll {
+  setPage: Dispatch<SetStateAction<number>>;
+  page: number;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+}
+
 export interface IUseInfiniteScroll {
   animes: IAnimeApi[];
   slug: string;
